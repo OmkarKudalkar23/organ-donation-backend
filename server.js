02744520cd0e4a56ed3a7af49731b96dc13e63b9
Omkar Kudalkar <omkarkudalkar2016@gmail.com>
2026-05-03 15:07:40 +0530

    cors fixed

--- server.js
+++ server.js
@@ -22,12 +22,16 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: '*', // Allow all origins for deployment flexibility
         methods: ['GET', 'POST']
     }
 });
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
